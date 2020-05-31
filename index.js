b9a6069a58de001efc4bbd16f72f9e011ab74218
Juan Camilo Arias <jcamiloat@gmail.com>
2020-05-31 17:37:40 -0500

    3.8 Setting up Morgan

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.use(express.json());
+app.use(
+  morgan('tiny', {
+    skip: (req, res) => req.method === 'POST',
+  })
+);
+
+morgan.token('type', (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :type',
+    {
+      skip: (req, res) => req.method !== 'POST',
+    }
+  )
+);
 
 const generateRandomId = () => {
   min = Math.ceil(0);
