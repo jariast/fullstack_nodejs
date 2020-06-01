d520a44078e7752d12c767ee5c839f0b8b8c5ad7
Juan Camilo Arias <jcamiloat@gmail.com>
2020-06-01 07:15:10 -0500

    Building frontend

---
+++ build/precache-manifest.1375c11e7f9f684d20def08e6a31fe5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa4894166bbf539894ed988c4bf392d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99a2c19e760aa62c1d2a",
+    "url": "/static/css/main.a9b2a8f7.chunk.css"
+  },
+  {
+    "revision": "266555d0d96dac7fa1bc",
+    "url": "/static/js/2.9e0805ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e0805ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a2c19e760aa62c1d2a",
+    "url": "/static/js/main.224e5755.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
